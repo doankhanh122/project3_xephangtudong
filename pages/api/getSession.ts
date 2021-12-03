dee6c6a366527f87add6974c7f27623096ee4def
doankhanh122 <60107916+doankhanh122@users.noreply.github.com>
2021-12-03 15:47:10 +0700

    Connected DATABASE successfully

---
+++ pages/api/getSession.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { dbConnection } from "./dbconnection";
+
+const sqlQuerry = "Select key from sessions";
+export default async function getSession(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  dbConnection.connect((error: any) => {
+    if (error) throw error;
+    console.log("Da ket noi database!");
+    dbConnection.query(
+      "Select * from sessions",
+      (err: any, results: any, fields: any) => {
+        if (err) throw err;
+        res.status(200).json(results[0]);
+      }
+    );
+  });
+}
